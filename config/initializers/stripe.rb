0ffba68d40614631d2a3061236a3aaaa20de3300
Alex Wheeler <afwheeler92@gmail.com>
2015-05-27 08:15:10 -0600

    updates keys from environemnt for stripe initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['stripe_publishable_key'],
-  :secret_key      => ENV['stripe_api_key']
+  :publishable_key => ENV['publishable_key'],
+  :secret_key      => ENV['secret_key']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
